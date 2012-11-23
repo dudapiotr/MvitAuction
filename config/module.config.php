1028c4e90408a707528642c220632b041935098a
Rickard Eriksson <rickard@mv-it.se>
2012-11-23 17:41:17 +0100

    First version of module.config.php

---
+++ config/module.config.php
@@ -0,0 +1,34 @@
+<?php
+// conﬁg/module.config.php:
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'mvitauction' => 'MVITAuction\Controller\AuctionController',
+        ),
+    ),
+
+    'router' => array(
+        'routes' => array(
+            'mvitauction' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/auctions[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'MVITAuction\Controller\Auction',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'mvitauction' => __DIR__ . '/../view',
+        ),
+    ),
+);
