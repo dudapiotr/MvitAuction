9bdc6f99747f8f05964b0037127ae5cff1e0cbe5
Rickard Eriksson <rickard@riceri.se>
2013-06-18 23:47:52 +0200

    Added function to get highest bid

--- src/MvitAuction/Model/BidTable.php
+++ src/MvitAuction/Model/BidTable.php
@@ -67,6 +67,27 @@ class BidTable extends AbstractTableGateway  {
         return $resultSet;
     }
 
+    public function getHighestBidByAuction($auction) {
+        $auction = (int) $auction;
+        $resultSet = $this->select(function (Select $select) use ($auction) {
+                $select->columns(array('id' => 'AB_Id',
+                                       'auction_id' => 'AB_AuctionId',
+                                       'user_id' => 'AB_UserId',
+                                       'bid' => 'AB_Bid',
+                                       'time' => 'AB_Time',
+                                      )
+                                )
+                       ->join('user', 'auction_bid.AB_UserId = user.user_id', array('username' => 'username',))
+                       ->where(array('AB_AuctionId' => $auction))
+                       ->order('auction_bid.AB_Bid DESC')->limit(1);
+            });
+        $row = $resultSet->current();$
+        if (!$row) {$
+            throw new \Exception("Could not find row $id");$
+        }$
+        return $row;$
+    }
+
     public function getBidsByUser($user) {
         $user = (int) $user;
         $resultSet = $this->select(function (Select $select) use ($user) {
