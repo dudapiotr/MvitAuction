8decbd458141e8939dccad419db456d74ee32bd1
Rickard Eriksson <rickard@riceri.se>
2013-06-27 00:27:52 +0200

    Custom fieldset for category and currency selectors

---
+++ src/MvitAuction/Form/CategoryFieldset.php
@@ -0,0 +1,29 @@
+<?php
+namespace MvitAuction\Form;
+
+use MvitAuction\Model;
+use Zend\Form\Fieldset;
+
+class CategoryFieldset extends Fieldset { 
+    public function __construct($categoryTable) {
+        parent::__construct('category'); 
+
+        $options = array();
+        foreach($categoryTable->fetchAll() as $categoryRow) {
+            $options[$categoryRow->id] = $categoryRow->name;
+        }
+var_dump($options);
+        $this->add(array(
+            'name' => 'category_id',
+            'type' => 'Zend\Form\Element\Select',
+            'attributes' => array(
+                'required' => true,
+            ),
+            'options' => array(
+                'label' => 'Category',
+                'options' => $options,
+            ),
+        ));
+
+    }
+}
---
+++ src/MvitAuction/Form/CurrencyFieldset.php
@@ -0,0 +1,29 @@
+<?php
+namespace MvitAuction\Form;
+
+use MvitAuction\Model;
+use Zend\Form\Fieldset;
+
+class CurrencyFieldset extends Fieldset { 
+    public function __construct($currencyTable) {
+        parent::__construct('currency');
+
+        $options = array();
+        foreach($currencyTable->fetchAll() as $currencyRow) {
+            $options[$currencyRow->id] = $currencyRow->name;
+        }
+    
+        $this->add(array(
+            'name' => 'currency_id',
+            'type' => 'Zend\Form\Element\Select',
+            'attributes' => array(
+                'required' => true,
+            ),
+            'options' => array(
+                'label' => 'Currency',
+                'options' => $options,
+            ),
+        ));
+
+    }
+}
